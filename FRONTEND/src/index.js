fb95d2cc09ebfc8bccb3a281994cd4225dee8810
Slacarta11 <slacargim@gmail.com>
2025-02-14 17:15:53 +0100

    añado frontend y backend

--- FRONTEND/src/index.js
+++ FRONTEND/src/index.js
@@ -1,9 +1,17 @@
 import axios from 'axios';
 
 window.readColonias = function(){
-    axios.get('http://localhost:8080/colonias')
+    axios.get('http://localhost:8081/colonias')
         .then((response) => {
-            const coloniaslist = responde.data;
+            const coloniaslist = response.data;
+            const coloniasUl= document.getElementById('colonias');
+
+            coloniaslist.forEach(colonia => {
+                const listItem = document.createElement('li');//crea un nuevo elemento HTML de tipo <li>. Este es un "contenedor" vacío
+                listItem.appendChild(document.createTextNode(colonia.brand + ' (' + colonia.name + ') ' + colonia.description));
+                coloniasUl.appendChild(listItem);
+                
+            });
 
         });
 }  
