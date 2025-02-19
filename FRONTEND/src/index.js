5ba5ffc1619ed057c27f35b10b68a3996a9a061e
Slacarta11 <slacargim@gmail.com>
2025-02-19 18:20:19 +0100

    añado cambios

--- FRONTEND/src/index.js
+++ FRONTEND/src/index.js
@@ -5,24 +5,13 @@ window.readColonias = function () { //creamos metodo readcolonias
     .get("http://localhost:8081/colonias") //endpoint de /colonias
     .then((response) => {
       const coloniaslist = response.data; //es la respuesta que da el backend al frontend
-      console.log(coloniaslist);
-
       const coloniasUl = document.getElementById("colonias"); //document para llamar al HTML, siempre que quieras llamar HTML
-      console.log(coloniasUl);
-
+      
       coloniaslist.forEach((colonia) => { //creamos metodo de array a la variable coloniaslist
-        console.log(colonia);
+        
         const listItem = document.createElement("li"); //crea un nuevo elemento HTML de tipo <li>. Este es un "contenedor" vacío
-        listItem.appendChild(
-          document.createTextNode( // document.createTextNode es una cadena de texto
-            colonia.Marca +
-              " " +
-              colonia.Materiales +
-              " " +
-              colonia.Nombre +
-              " " +
-              colonia.ID
-          )
+        listItem.className = 'list-group-item'
+        listItem.appendChild(document.createTextNode(colonia.Marca + " " + colonia.Materiales + " " + colonia.Nombre + " " + colonia.ID)// document.createTextNode es una cadena de texto   
         );
         coloniasUl.appendChild(listItem); //appendchild es para añadir lo que este entre parentesis
       });
